3aff175d1dbd5217038186b35fd069e915d82552
RoqueDeicide <roquegodslayer@hotmail.com>
2014-09-16 00:41:13 +0400

    Moved a list of supported IWAD files to separate class, also added list of episodic games.

---
+++ Launcher/Iwads.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+	/// <summary>
+	/// Defines arrays of supported IWAD files.
+	/// </summary>
+	public static class Iwads
+	{
+		/// <summary>
+		/// A list of supported IWAD files.
+		/// </summary>
+		public static readonly SortedList<string, string> SupportedIwads =
+			new SortedList<string, string>
+		{
+			{"doom1.wad", "Doom Shareware version"},
+			{"doom.wad", "Doom Full version"},
+			{"doomu.wad", "Ultimate Doom"},
+			{"bfgdoom.wad", "Doom BFG Edition"},
+			{"freedoom1.wad", "Freedoom: Phase 1"},
+			{"doom2.wad", "Doom 2"},
+			{"bfgdoom2.wad", "Doom 2 BFG Edition"},
+			{"tnt.wad", "Final Doom — TNT: Evilution"},
+			{"plutonia.wad", "Final Doom — The Plutonia Experiment"},
+			{"freedm.wad", "FreeDM"},
+			{"freedoom2.wad", "Freedoom: Phase 2"},
+			{"doom2f.wad", "French Doom II"},
+			{"heretic1.wad", "Heretic (Shareware)"},
+			{"heretic.wad", "Heretic (Registered or Commercial)"},
+			{"blasphemer.wad", "Blasphemer"},
+			{"hexendemo.wad", "Hexen (Demo)"},
+			{"hexen.wad", "Hexen (Full)"},
+			{"hexdd.wad", "Hexen: Deathkings of the Dark Citadel"},
+			{"strife0.wad", "Strife (Teaser)"},
+			{"strife.wad", "Strife (Full)"},
+			{"chex.wad", "Chex Quest"},
+			{"chex3.wad", "Chex Quest 3"},
+			{"action2.wad", "Action Doom 2: Urban Brawl"},
+			{"harm1.wad", "Harmony v1.1"},
+			{"hacx.wad", "Hacx v1.2"},
+			{"hacx2.wad", "Hacx v2.0"}
+		};
+		/// <summary>
+		/// A list of episodic games.
+		/// </summary>
+		public static readonly string[] EpisodicIwads =
+		{
+			"doom1.wad",
+			"doom.wad",
+			"bfgdoom.wad",
+			"heretic1.wad",
+			"heretic.wad",
+			"chex.wad"
+		};
+		/// <summary>
+		/// Finds IWAD files this launcher can load, that are also present in the base directory.
+		/// </summary>
+		/// <returns>A list of IWAD file names.</returns>
+		public static IEnumerable<string> FindSupportedIwads()
+		{
+			// Scan for IWADs in the app folder.
+			string currentFolder = AppDomain.CurrentDomain.BaseDirectory;
+			return
+				Iwads.SupportedIwads.Keys.Where(x => File.Exists(Path.Combine(currentFolder, x)));
+		}
+	}
+}
--- Launcher/MainWindow.xaml.cs
+++ Launcher/MainWindow.xaml.cs
@@ -28,35 +28,6 @@ namespace Launcher
 	/// </summary>
 	public partial class MainWindow
 	{
-		private static readonly SortedList<string, string> supportedIwads = new SortedList<string, string>
-		{
-			{"doom1.wad", "Doom Shareware version"},
-			{"doom.wad", "Doom Full version"},
-			{"doomu.wad", "Ultimate Doom"},
-			{"bfgdoom.wad", "Doom BFG Edition"},
-			{"freedoom1.wad", "Freedoom: Phase 1"},
-			{"doom2.wad", "Doom 2"},
-			{"bfgdoom2.wad", "Doom 2 BFG Edition"},
-			{"tnt.wad", "Final Doom — TNT: Evilution"},
-			{"plutonia.wad", "Final Doom — The Plutonia Experiment"},
-			{"freedm.wad", "FreeDM"},
-			{"freedoom2.wad", "Freedoom: Phase 2"},
-			{"doom2f.wad", "French Doom II"},
-			{"heretic1.wad", "Heretic (Shareware)"},
-			{"heretic.wad", "Heretic (Registered or Commercial)"},
-			{"blasphemer.wad", "Blasphemer"},
-			{"hexendemo.wad", "Hexen (Demo)"},
-			{"hexen.wad", "Hexen (Full)"},
-			{"hexdd.wad", "Hexen: Deathkings of the Dark Citadel"},
-			{"strife0.wad", "Strife (Teaser)"},
-			{"strife.wad", "Strife (Full)"},
-			{"chex.wad", "Chex Quest"},
-			{"chex3.wad", "Chex Quest 3"},
-			{"action2.wad", "Action Doom 2: Urban Brawl"},
-			{"harm1.wad", "Harmony v1.1"},
-			{"hacx.wad", "Hacx v1.2"},
-			{"hacx2.wad", "Hacx v2.0"}
-		};
 		private LaunchConfiguration config;
 		private string file;
 		private VistaOpenFileDialog selectExtraFilesDialog;
