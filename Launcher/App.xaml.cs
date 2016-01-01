836a26742a814461d0be7065d769bb4711784a3c
RoqueDeicide <roquegodslayer@hotmail.com>
2016-01-01 11:39:50 +0400

    Information about the unhandled exceptions is now reported into the log.

--- Launcher/App.xaml.cs
+++ Launcher/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Launcher.Logging;
 
 namespace Launcher
 {
@@ -13,5 +14,34 @@ namespace Launcher
 	/// </summary>
 	public partial class App
 	{
+		private void StartUp(object sender, StartupEventArgs e)
+		{
+			AppDomain.CurrentDomain.UnhandledException += (o, args) =>
+			{
+				Log.Error("An error has occurred:");
+
+				Exception ex = args.ExceptionObject as Exception;
+
+				if (ex == null)
+				{
+					Log.Error("Unknown format of the error: additional information cannot be provided.");
+				}
+				else
+				{
+					Log.Error(ex.Message);
+					Log.Error(ex.StackTrace);
+
+					Exception inner = ex.InnerException;
+					while (inner != null)
+					{
+						Log.Error("Caused by:");
+						Log.Error(inner.Message);
+						Log.Error(inner.StackTrace);
+
+						inner = inner.InnerException;
+					}
+				}
+			};
+		}
 	}
 }
