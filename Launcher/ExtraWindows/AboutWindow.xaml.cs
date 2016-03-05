b709f976c08c8717c4c65f017dfb616cfb89547a
RoqueDeicide <roquegodslayer@hotmail.com>
2016-03-06 03:28:06 +0400

    Fixed the link in About window not being clickable.

--- Launcher/ExtraWindows/AboutWindow.xaml.cs
+++ Launcher/ExtraWindows/AboutWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace Launcher
 {
@@ -33,6 +34,16 @@ namespace Launcher
 
 			// ReSharper disable once ExceptionNotDocumented
 			this.AssemblyFileVersionTextBox.Text = FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+		}
+		private void OpenBrowser(object sender, RoutedEventArgs e)
+		{
+			Hyperlink link = sender as Hyperlink;
+			if (link == null)
+			{
+				return;
+			}
+
+			Process.Start(link.NavigateUri.ToString());
 		}
 	}
 }
