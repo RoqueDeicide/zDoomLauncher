7721b2eac3cfef6d9d3585b82ab746676c4ec5ec
RoqueDeicide <roquegodslayer@hotmail.com>
2016-03-08 08:12:27 +0400

    CommandLineWindow now display length of the command line along with indication whether the latter is too long.

--- Launcher/CommandLineWindow.xaml.cs
+++ Launcher/CommandLineWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace Launcher
 {
@@ -16,6 +18,17 @@ namespace Launcher
 		{
 			this.InitializeComponent();
 			this.CommandLineTextBox.Text = line;
+
+			Paragraph paragraph = new Paragraph();
+
+			paragraph.Inlines.Add(new Run("Number of characters in the command line is equal to "));
+			paragraph.Inlines.Add(new Bold(new Run(line.Length.ToString()))
+			{
+				Foreground = line.Length > 2080 ? Brushes.Red : Brushes.Green
+			});
+			paragraph.Inlines.Add(new Run("."));
+
+			this.CharacterCountTextBox.Document = new FlowDocument(paragraph);
 		}
 	}
 }
