705a8fec34fc1193b8f556e0e8078ac47ba15d53
RoqueDeicide <roquegodslayer@hotmail.com>
2020-06-08 14:31:17 +0400

    Converted a small lambda into named method.

--- Launcher/App.xaml.cs
+++ Launcher/App.xaml.cs
@@ -13,31 +13,7 @@ namespace Launcher
 	{
 		private void StartUp(object sender, StartupEventArgs e)
 		{
-			AppDomain.CurrentDomain.UnhandledException += (o, args) =>
-														  {
-															  Log.Error("An error has occurred:");
-
-															  if (!(args.ExceptionObject is Exception ex))
-															  {
-																  Log
-																	 .Error("Unknown format of the error: additional information cannot be provided.");
-															  }
-															  else
-															  {
-																  Log.Error(ex.Message);
-																  Log.Error(ex.StackTrace);
-
-																  var inner = ex.InnerException;
-																  while (inner != null)
-																  {
-																	  Log.Error("Caused by:");
-																	  Log.Error(inner.Message);
-																	  Log.Error(inner.StackTrace);
-
-																	  inner = inner.InnerException;
-																  }
-															  }
-														  };
+			AppDomain.CurrentDomain.UnhandledException += OnUnhandledExceptionInCurrentDomain;
 
 			// Make sure that the working directory is not screwed up when launching the app from the link.
 			var exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -47,6 +23,31 @@ namespace Launcher
 			}
 
 			Directory.SetCurrentDirectory(exePath);
+		}
+
+		private static void OnUnhandledExceptionInCurrentDomain(object o, UnhandledExceptionEventArgs args)
+		{
+			Log.Error("An error has occurred:");
+
+			if (!(args.ExceptionObject is Exception ex))
+			{
+				Log.Error("Unknown format of the error: additional information cannot be provided.");
+			}
+			else
+			{
+				Log.Error(ex.Message);
+				Log.Error(ex.StackTrace);
+
+				var inner = ex.InnerException;
+				while (inner != null)
+				{
+					Log.Error("Caused by:");
+					Log.Error(inner.Message);
+					Log.Error(inner.StackTrace);
+
+					inner = inner.InnerException;
+				}
+			}
 		}
 	}
 }
