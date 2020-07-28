e7aedc46cc0105227d577cdc698ec18a55265584
RoqueDeicide <roquegodslayer@hotmail.com>
2020-07-28 16:32:27 +0400

    Rejigged the Directories window.

--- Launcher/ExtraWindows/Directories.xaml.cs
+++ Launcher/ExtraWindows/Directories.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System.ComponentModel;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 using Ookii.Dialogs.Wpf;
 
 namespace Launcher
@@ -25,191 +22,63 @@ namespace Launcher
 						  };
 
 			this.InitializeComponent();
-
-			for (int i = 0; i < ExtraFilesLookUp.Directories.Count; i++)
-			{
-				string directory = ExtraFilesLookUp.Directories[i];
-
-				this.DirectoriesListBox.Items.Add(this.CreateDirectoryName(directory, i));
-			}
-
-			// Add empty row, if there are no directories in the list.
-			if (ExtraFilesLookUp.Directories.Count == 0)
-			{
-				this.DirectoriesListBox.Items.Add(this.CreateDirectoryName("", -1));
-			}
 		}
 
-		private DirectoryName CreateDirectoryName(string path, int index)
+		private void AddDirectoryNoSubDirectories(object sender, RoutedEventArgs e)
 		{
-			var name = new DirectoryName
-					   {
-						   Path  = path,
-						   Index = index
-					   };
-			name.PropertyChanged += this.DirectoryNameChanged;
-			return name;
+			this.AddDirectory(false);
 		}
 
-		private void DirectoryNameChanged(object sender, PropertyChangedEventArgs e)
+		private void AddDirectoryWithSubDirectories(object sender, RoutedEventArgs e)
 		{
-			if (!(sender is DirectoryName directory))
-			{
-				return;
-			}
+			this.AddDirectory(true);
+		}
 
-			if (e.PropertyName != "Path")
+		private void AddDirectory(bool withSubFolders)
+		{
+			if (this.dialog.ShowDialog() == true && Directory.Exists(this.dialog.SelectedPath))
 			{
-				return;
-			}
+				string path = this.dialog.SelectedPath;
 
-			// Remove the directory from the observable collection if new path doesn't exist.
-			if (!directory.Exists && directory.Index > -1)
-			{
-				this.RemovePath(directory.Index);
+				AddDirectory(path);
 
-				return;
-			}
-
-			// Add the directory to the collection, if there is no such directory in it.
-			if (directory.Exists && directory.Index == -1)
-			{
-				if (ExtraFilesLookUp.Directories.Any(x => x == directory.Path))
+				if (withSubFolders)
 				{
-					return;
+					foreach (string directory in Directory.EnumerateDirectories(path, "*", SearchOption.AllDirectories))
+					{
+						AddDirectory(directory);
+					}
 				}
+			}
+		}
 
-				int senderIndex = this.DirectoriesListBox.Items.IndexOf(directory);
-				if (senderIndex == 0)
-				{
-					// Insert at the start of global collection.
-					this.InsertPath(0, directory);
-				}
-				else if (senderIndex == this.DirectoriesListBox.Items.Count - 1)
-				{
-					directory.Index = ExtraFilesLookUp.Directories.Count;
-					// Just add to the end.
-					ExtraFilesLookUp.Directories.Add(directory.Path);
-				}
-				else
-				{
-					// Insert somewhere between directories this one is in between.
-					var next = this.DirectoriesListBox.Items[senderIndex + 1] as DirectoryName;
+		private static void AddDirectory(string directory)
+		{
+			var dirList = new List<string>(ExtraFilesLookUp.Directories);
 
-					Debug.Assert(next != null);
+			int index = dirList.BinarySearch(directory);
 
-					this.InsertPath(next.Index, directory);
-				}
-
+			if (index >= 0)
+			{
+				// We already have it.
 				return;
 			}
 
-			if (directory.Exists && directory.Index > -1)
-			{
-				// Replace the path.
-				ExtraFilesLookUp.Directories[directory.Index] = directory.Path;
-			}
-		}
-
-		private void OpenFolderDialog(object sender, RoutedEventArgs e)
-		{
-			var button = sender as Button;
-
-			if (!(button?.DataContext is DirectoryName name))
-			{
-				return;
-			}
-
-			this.dialog.SelectedPath = name.Exists ? name.Path : "";
-
-			if (this.dialog.ShowDialog(this) == true && Directory.Exists(this.dialog.SelectedPath))
-			{
-				name.Path = this.dialog.SelectedPath;
-			}
-		}
-
-		private void InsertDirectoryAbove(object sender, RoutedEventArgs e)
-		{
-			this.InsertRow(sender, 0);
-		}
-
-		private void InsertDirectoryBelow(object sender, RoutedEventArgs e)
-		{
-			this.InsertRow(sender, 1);
-		}
-
-		private void InsertRow(object sender, int offset)
-		{
-			var element = sender as FrameworkElement;
-
-			if (!(element?.DataContext is DirectoryName name))
-			{
-				return;
-			}
-
-			int senderIndex = this.DirectoriesListBox.Items.IndexOf(name);
-			this.DirectoriesListBox.Items.Insert(senderIndex + offset, this.CreateDirectoryName("", -1));
+			// Insert where necessary to maintain a sorted sequence.
+			ExtraFilesLookUp.Directories.Insert(~index, directory);
 		}
 
 		private void RemoveDirectory(object sender, RoutedEventArgs e)
 		{
 			var element = sender as FrameworkElement;
 
-			if (!(element?.DataContext is DirectoryName name))
+			if (!(element?.DataContext is string name))
 			{
 				return;
 			}
-
-			// Clear the text box, if this is only one row.
-			if (this.DirectoriesListBox.Items.Count == 1)
-			{
-				name.Path = "";
-				return;
-			}
-
-			if (name.Index > -1)
-			{
-				this.RemovePath(name.Index);
-			}
-
+			
 			// Remove the row.
-			this.DirectoriesListBox.Items.Remove(name);
-		}
-
-		private void InsertPath(int index, DirectoryName name)
-		{
-			ExtraFilesLookUp.Directories.Insert(index, name.Path);
-
-			// Adjust indexes.
-			foreach (object directoryNameItem in this.DirectoriesListBox.Items)
-			{
-				var directory = directoryNameItem as DirectoryName;
-
-				Debug.Assert(directory != null);
-
-				if (directory.Index >= name.Index)
-				{
-					directory.Index++;
-				}
-			}
-
-			name.Index = index;
-		}
-
-		private void RemovePath(int index)
-		{
-			ExtraFilesLookUp.Directories.RemoveAt(index);
-
-			// Adjust indexes.
-			foreach (object directoryNameItem in this.DirectoriesListBox.Items)
-			{
-				var directory = directoryNameItem as DirectoryName;
-
-				if (directory?.Index > index)
-				{
-					directory.Index--;
-				}
-			}
+			ExtraFilesLookUp.Directories.Remove(name);
 		}
 	}
 }
